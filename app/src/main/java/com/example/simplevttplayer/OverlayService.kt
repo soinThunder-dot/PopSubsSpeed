9213243a024b2b47016fab76e06b4ff4296ecd8e
5thunderHEARTpirece <sointangho@gmail.com>
2026-05-27 12:52:43 +0800

    import 60GT is error

--- app/src/main/java/com/example/simplevttplayer/OverlayService.kt
+++ app/src/main/java/com/example/simplevttplayer/OverlayService.kt
@@ -27,7 +27,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
-impor60Gt android.widget.TextView
+import android.widget.TextView
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.simplevttplayer.JpGrammarHighlighter
 import android.widget.Spinner
