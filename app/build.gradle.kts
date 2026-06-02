000e75603f684d1e99a0aa15d7b4c7971f73cf12
5thunderHEARTpirece <sointangho@gmail.com>
2026-06-02 15:00:31 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-                applicationId = "com.example.simplevttplayer.easyview4"
+                applicationId = "com.example.simplevttplayer.easyview5"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "4"
-
+        versionCode = 5
+        versionName = "5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
