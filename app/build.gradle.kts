6dc0f7090dbf69e3bdc3f892038e787f7ef2832f
5thunderHEARTpirece <sointangho@gmail.com>
2026-06-08 13:01:53 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-                applicationId = "com.example.simplevttplayer.easyview__2"
+                applicationId = "com.example.simplevttplayer.easyview_2"
         minSdk = 21
         targetSdk = 35
         versionCode = 2
