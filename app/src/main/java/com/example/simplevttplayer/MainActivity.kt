68581fccfc946726df4d8940659eb1082e4ac32c
5thunderHEARTpirece <sointangho@gmail.com>
2026-06-08 15:13:14 +0800

    buttonLaunchOverlay.performClick()*

--- app/src/main/java/com/example/simplevttplayer/MainActivity.kt
+++ app/src/main/java/com/example/simplevttplayer/MainActivity.kt
@@ -575,7 +575,7 @@ class MainActivity : AppCompatActivity() {
             if (isOverlayUIShown && checkOverlayPermission()) {     startOverlayService()    ;    sendSubtitleUpdate(textViewSubtitle.text.toString())    }  }
     }
     private val overlayCloseReceiver = object : BroadcastReceiver() {//overlay close 加 receiver 接廣播，呼叫 resetPlayback()
-        override fun onReceive(context: Context?, intent: Intent?) { if (intent?.action == OverlayService.ACTION_OVERLAY_CLOSE) {  buttonLaunchOverlay()  }   }
+        override fun onReceive(context: Context?, intent: Intent?) { if (intent?.action == OverlayService.ACTION_OVERLAY_CLOSE) {  buttonLaunchOverlay.performClick()  }   }
     }
     
     private fun findCueForTime(time: Long): SubtitleCue? = subtitleCues.find { time >= it.startTimeMs && time < it.endTimeMs }
